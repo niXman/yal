90ac3f8d547eabb3f12ea54c99ce598632f58900
niXman <niXman@users.noreply.github.com>
2013-11-20 16:45:07 +0300

    Create main.cpp

---
+++ examples/usertype/main.cpp
@@ -0,0 +1,26 @@
+
+#include <yal/yal.hpp>
+
+#include <iostream>
+
+/***************************************************************************/
+
+struct user_point {
+	int x, y;
+
+	friend std::ostream& operator<< (std::ostream &os, const user_point &p) {
+		return os << "{x:" << p.x << ",y:" << p.y << '}';
+	}
+};
+
+/***************************************************************************/
+
+int main() {
+	YAL_CREATE(session1, "sessions/session1")
+	YAL_SESSION_TO_TERM(session1, true, "terminal")
+
+	user_point p = {34, 65};
+	YAL_LOG_INFO(session1, "%1%", p)
+}
+
+/***************************************************************************/
